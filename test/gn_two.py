610d9dc6c5b3dcbb26e9087604a98fadb9c8bff1
minha721 <2minha721@gmail.com>
2022-04-29 21:53:33 +0900

    [ADD] Add gitignore

--- test/gn_two.py
+++
@@ -1,9 +0,0 @@
-def solution(N):
-    str_N = str(N)
-    list_N = [i for i in str_N]
-
-    asc_num = int(''.join(sorted(list_N)))
-    desc_num = int(''.join(sorted(list_N, reverse=True)))
-
-    answer = asc_num + desc_num
-    return answer
